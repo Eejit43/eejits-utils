5a7833fa78d7724215a0a282aeb963f32cf2fdc2
Will <76887639+Eejit43@users.noreply.github.com>
2026-03-15 09:45:09 -0400

    chore: rm eslint config from ignore

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,6 @@
 import sharedConfig from '@eejit/eslint-config-typescript';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default defineConfig(sharedConfig, globalIgnores(['eslint.config.js', 'source/*']), {
+export default defineConfig(sharedConfig, globalIgnores(['source/*']), {
     languageOptions: { parserOptions: { project: ['./tsconfig.json'] } },
 });
